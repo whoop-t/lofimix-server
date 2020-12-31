298ae427dadafd1ee8986f3d9c2966da9cfd24f3
Tommy Mallis <tommymallis@gmail.com>
2020-12-30 16:24:48 -0800

    Atlas DB connection created and added, remove log

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ const config = require('./config/config');
 const logger = require('./config/logger');
 
 let server;
-server = app.listen(config.port, () => {
-  logger.info(`Listening to port ${config.port}`);
+mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+  logger.info('Connected to MongoDB');
+  server = app.listen(config.port, () => {
+    logger.info(`Listening to port ${config.port}`);
+  });
 });
-// mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
-//   logger.info('Connected to MongoDB');
-
-// });
 
 const exitHandler = () => {
   if (server) {
