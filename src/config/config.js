3ef27de15202ce76e9011022a3c800afab9d125c
Tommy Mallis <tommymallis@gmail.com>
2021-01-02 02:15:41 -0800

    S3 Bucket uploads working

--- src/config/config.js
+++ src/config/config.js
@@ -9,6 +9,8 @@ const envVarsSchema = Joi.object()
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
     PORT: Joi.number().default(3000),
     MONGODB_URL: Joi.string().required().description('Mongo DB url'),
+    AWS_ACCESS_KEY_ID: Joi.string().required().description('AWS Access key'),
+    AWS_SECRET_KEY: Joi.string().required().description('AWS secret key'),
     JWT_SECRET: Joi.string().required().description('JWT secret key'),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
     JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
@@ -36,6 +38,10 @@ module.exports = {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     },
+  },
+  aws: {
+    access: envVars.AWS_ACCESS_KEY_ID,
+    secret: envVars.AWS_SECRET_KEY,
   },
   jwt: {
     secret: envVars.JWT_SECRET,
--- src/middlewares/uploadFile.js
+++ src/middlewares/uploadFile.js
@@ -1,26 +1,43 @@
 const multer = require('multer');
+const AWS = require('aws-sdk');
+const fs = require('fs');
 const path = require('path');
+const config = require('../config/config');
 const ApiError = require('../utils/ApiError');
 const httpStatus = require('http-status');
+
+/**
+ * AWS Setup
+ */
+AWS.config.update({
+  accessKeyId: config.aws.access,
+  secretAccessKey: config.aws.secret,
+  region: 'us-west-1',
+});
+
+const s3 = new AWS.S3();
+
+/**
+ * Multer Setup
+ */
+// SET STORAGE
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+  limits: {
+    fileSize: 25000000,
+  },
+});
 
 /**
  * Upload file middleware
  */
 // TODO needs error checking and verification or something
 const uploadFile = (req, res, next) => {
-  // SET STORAGE
-  const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname + '-' + Date.now());
-    },
-    limits: {
-      fileSize: 25000000,
-    },
-  });
-
   const upload = multer({
     storage: storage,
     fileFilter: multerFilter,
@@ -30,7 +47,8 @@ const uploadFile = (req, res, next) => {
     if (err) {
       return res.json({ success: false, err: err.message });
     }
-    next();
+    uploadToAWS(req.file.path, req.file.filename, res, next);
+    //next();
   });
 };
 
@@ -42,6 +60,55 @@ const multerFilter = (req, file, cb) => {
   }
   cb(null, true);
 };
+
+/**
+ * Upload file to S3 Bucket
+ * @param {*} source
+ * @param {*} targetName
+ * @param {*} res
+ * @param {*} next
+ */
+const uploadToAWS = (source, targetName, res, next) => {
+  console.log('preparing to upload...');
+  fs.readFile(source, function (err, filedata) {
+    if (!err) {
+      const putParams = {
+        Bucket: 'lofimix-tracks',
+        Key: targetName,
+        Body: filedata,
+      };
+      s3.upload(putParams, function (err, data) {
+        if (err) {
+          console.log('Could not upload the file. Error :', err);
+          return res.send({ success: false });
+        } else {
+          //fs.unlink(source); // Deleting the file from uploads folder(Optional).Do Whatever you prefer.
+          console.log('Successfully uploaded the file');
+          next();
+        }
+      });
+    } else {
+      console.log({ err: err });
+    }
+  });
+};
+
+//TODO Figure out how to retrieve tracks from S3
+// const retrieveTrackForAWS = (filename,res) => {
+//   const getParams = {
+//     Bucket: 'lofimix-tracks',
+//     Key: filename
+//   };
+
+//   s3.getObject(getParams, function(err, data) {
+//     if (err){
+//       return res.status(400).send({success:false,err:err});
+//     }
+//     else{
+//       return res.send(data.Body);
+//     }
+//   });
+// }
 
 module.exports = {
   uploadFile,
